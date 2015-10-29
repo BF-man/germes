143a584ac377e000c2378eca0211ba4d238641e5
Artem Bubnov <bf-man@mail.ru>
2015-10-29 15:36:49 +0300

    signed user can edit showbill

--- app/helpers/showbill_helper.rb
+++ app/helpers/showbill_helper.rb
@@ -1,11 +1,2 @@
-require 'rails_autolink'
-
 module ShowbillHelper
-  def showbill_tag(id, content)
-    if current_user.present? && current_user.director?
-      text_area_tag("showbill", content,  {class: "showbill width-max-avail", id: id})
-    else
-      content_tag("div", auto_link(content))
-    end
-  end
 end
